88f80de31c51a8816beeac427a38f690e3930132
Niakrist <ak_88dron@inbox.ru>
2026-05-13 16:32:25 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@niakrist/ui-kit";
+
+function App() {
+  return (
+    <>
+      <Button appearance="primary" size="large">
+        Button
+      </Button>
+      <Button size="medium">Button</Button>
+      <Button appearance="primary" size="small">
+        Button
+      </Button>
+    </>
+  );
+}
+
+export default App;
